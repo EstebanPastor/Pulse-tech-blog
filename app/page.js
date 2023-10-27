12618b15a5f94ce4d9b745deafb43d2ad3af4cae
EstebanPastor <estebanandrespastor@gmail.com>
2023-10-27 15:47:56 -0300

    feat: :art: Add postcards
    Now the user can see which user posts and what are their posts

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Hero from "./components/ui/hero/Hero";
+import LatestPost from "./components/ui/latest-posts/LatestsPosts"
 
 function Home() {
   return (
     <>
       <div className="dark:text-white">
         <Hero />
+        <LatestPost />
       </div>
     </>
   );
